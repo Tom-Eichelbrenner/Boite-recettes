2e4171274e765ecc9add266ae50296bca68d8c49
Tom-Eichelbrenner <tom.eichelbrenner01@gmail.com>
2021-01-25 16:13:03 +0400

    loop over ingredients and instructions to display them on li's :)

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,26 @@
 import React from 'react';
 
 const Card = ({details}) => {
+    const ingredients = details.ingredients
+        .split(',')
+        .map(item => <li key={item}>{item}</li>)
+    const instructions = details.instructions
+        .split('\n')
+        .map(instruction => <li key={instruction}>{instruction}</li>)
     return (
         <div>
             <div className="card">
+                <div className="image">
+                    <img src={require(`../img/${details.image}`)} alt={details.nom}/>
+                </div>
                 <div className="recette">
                     <h2>{details.nom}</h2>
+                    <ul className="liste-ingredients">
+                        {ingredients}
+                    </ul>
+                    <ol className="liste-instructions">
+                        {instructions}
+                    </ol>
                 </div>
             </div>
         </div>
