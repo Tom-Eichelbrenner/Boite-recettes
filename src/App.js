eb10a9599fd4b44e963469b08a65c3888c24aa69
Tom-Eichelbrenner <tom.eichelbrenner01@gmail.com>
2021-01-25 15:43:06 +0400

    created admin to load state with recipes

--- src/App.js
+++ src/App.js
@@ -3,19 +3,33 @@ import React, { Component } from 'react'
 import './App.css'
 
 import Header from "./components/Header";
+import recettes from "./recettes";
+import Admin from "./components/Admin";
 
 class App extends Component {
   state = {
-    pseudo: this.props.match.params.pseudo
+    pseudo: this.props.match.params.pseudo,
+    recettes: {}
   }
 
   render () {
     return (
       <div className='box'>
         <Header pseudo={this.state.pseudo}/>
+        <div className="cards">
+          <div className="card">
+            <h2>Une carte</h2>
+          </div>
+        </div>
+        <Admin
+          loadExample={this.loadExample}/>
       </div>
     )
   }
+
+  loadExample = () => {
+    this.setState({recettes})
+  };
 }
 
 export default App
